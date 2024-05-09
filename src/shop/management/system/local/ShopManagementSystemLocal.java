71f4382a188bc0d53ca0790b6efbead335152738
Unsolicited2 <kirstencomputerscience40@gmail.com>
2024-05-09 19:57:30 -0400

    Add files via upload

--- src/shop/management/system/local/ShopManagementSystemLocal.java
+++ src/shop/management/system/local/ShopManagementSystemLocal.java
@@ -1,47 +1,46 @@
 package shop.management.system.local;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ShopManagementSystemLocal {
 
     
     public static void main(String[] args) {
-        canner input = new Scanner(System.in); 
-        
-        
-        System.out.print("Enter product name:");
-        String prod_name = input.next();
-
-        System.out.print("Enter product ID:");
-         int ID = input.nextInt();
-        
-        System.out.print("Enter product description:");
-        String description = input.next();
-        
-        System.out.print("Enter product quntity:");
-         int amount = input.nextInt();
-         
-         System.out.print("Enter value per product:");
-         double value = input.nextDouble();
-         
-         double totalValue = amount * value;
-
-        File file = new File("C:/Inventory/inventory.txt");                                // this one too
-        PrintWriter pw = new PrintWriter("C:/Inventory/inventory.txt");                  //file change it to what u storing it in Damion 
-        
-         pw.println("Product Name: " + prod_name);
-            pw.println("Description: " + description);
-            pw.println("Quantity: " + amount);
-            pw.println("value: " + value + amount);
-            pw.println("Total Inventory Value: " + totalValue);
-            pw.println("ID: " + ID);
-    
-             
-        System.out.print(prod_name+ " "+ description+" "+ amount +" "+totalValue+" has been added to Inventory");
-        pw.close();
+       
     }
-    //hello
+    public static void InventoryManager() throws FileNotFoundException {
+  //  String code, name;
+    
+  //  double price;
+     Scanner input = new Scanner(System.in);
+     try(FileWriter f = new FileWriter(":C/Temp/StoreInventory.txt", true)){
+             
+    // PrintWriter pw= new PrintWriter(":C/Temp/StoreInventory.txt");)
+             char addmore;
+              do {
+                System.out.print("Enter Product's code: ");
+                String code = input.next();
+                System.out.print("Enter Product's name: ");
+                String name = input.next();
+                System.out.print("Enter Product's price: ");
+                double price = input.nextDouble();
+                  System.out.println("Enter Product's amount:");
+                int quantity = input.nextInt();
+                
+                f.write(String.format("%s %s %.2f %d%n", code, name, price, quantity));
+                System.out.printf("%s %s has been added to your inventory list.%n", code, name);
+
+                System.out.print("Do you want to add more products? (y/n): ");
+                addmore = input.next().charAt(0);
+            } while (addmore == 'y' || addmore == 'Y');
+        } catch (IOException e) {
+            System.err.println("Error writing to the file: " + e.getMessage());
+        }
+    }
 }
+    
+            
+
+
